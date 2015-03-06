f77ecf6d26df96dceb4840d80a0a397ae77f7161
Kaiyuan Liang <liangkaiyuan@hotmail.com>
2015-03-06 21:23:37 +0800

    commit empty config file

---
+++ infra/config.py
@@ -0,0 +1,15 @@
+database_config = {
+    'username':'',
+    'password':r'',
+    'host':'',
+    'database':''
+}
+
+mail_config = {
+    'smtp_server':'',
+    'smtp_port':'',
+    'username':'',
+    'password':r'',
+    'from':'',
+    'to':''
+}
